7aad536f23c05d35cec7cbf15d151e72239480ee
Makreig <mark@i-lateral.com>
2018-09-17 13:19:14 +0100

    fixed tax calculation on line items

--- src/extensions/LineItemExtension.php
+++ src/extensions/LineItemExtension.php
@@ -10,4 +10,25 @@ class LineItemExtension extends DataExtension
     private static $belongs_many_many = [
         'Discounts' => AppliedDiscount::class
     ];
+
+    /**
+     * Get the amount of tax for a single unit of this item
+     *
+     * @return float
+     */
+    public function updateUnitTax(&$total)
+    {
+        $total = (($this->owner->UnitPrice - $this->getDiscountAmount()) / 100) * $this->owner->TaxRate;
+
+        return $total;
+    }
+
+    public function getDiscountAmount()
+    {
+        $parent = $this->owner->Parent();
+        $total = $parent->getDiscountAmount();
+        $count = $parent->getTotalItems();
+
+        return $total/$count;
+    }
 }
