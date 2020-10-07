c21b4aff8880ac6a6a0d752f0f6ed5abc896e8ba
Morven Lewis-Everley <morven@ilateral.co.uk>
2020-10-07 09:36:01 +0100

    All selecting of pre-defined and valid discounts on creation and auto set title/value

--- src/model/AppliedDiscount.php
+++ src/model/AppliedDiscount.php
@@ -3,9 +3,11 @@
 namespace SilverCommerce\Discounts\Model;
 
 use SilverStripe\ORM\DataObject;
+use SilverStripe\Forms\DropdownField;
 use SilverCommerce\Discounts\DiscountFactory;
 use SilverCommerce\OrdersAdmin\Model\Estimate;
 use SilverCommerce\OrdersAdmin\Model\LineItem;
+use SilverCommerce\Discounts\Model\DiscountCode;
 
 /**
  * A discount that has been applied to an Estimate/Invoice
@@ -16,8 +18,8 @@ class AppliedDiscount extends DataObject
     private static $table_name = 'AppliedDiscount';
 
     private static $db = [
-        'Title' => 'Varchar',
         'Code' => 'Varchar',
+        'Title' => 'Varchar',
         'Value' => 'Currency'
     ];
 
@@ -30,8 +32,8 @@ class AppliedDiscount extends DataObject
     ];
 
     private static $summary_fields = [
-        'Title',
         'Code',
+        'Title',
         'Value'
     ];
 
@@ -40,19 +42,71 @@ class AppliedDiscount extends DataObject
         return $this->calculateValue($item->Estimate()->getTotal());
     }
 
-    public function updateDiscount()
+    /**
+     * Automatically update this discount's value based on it's estimate
+     *
+     * @param bool $write Should this discount be written
+     * 
+     * @return self
+     */
+    public function updateDiscount($write = true)
     {
         $discount = $this->getDiscount();
         if ($discount->exists()) {
             $value = $discount->appliedAmount($this);
 
             $this->Value = $value;
-            $this->write();
+
+            if ($write) {
+                $this->write();
+            }
         }
+
+        return $this;
     }
 
     public function getDiscount()
     {
         return DiscountFactory::create($this->Code)->getDiscount(false);
+    }
+
+    /**
+     * Create a slightly nicer UI for setting up new discounts via the admin
+     *
+     * {@inheritDoc}
+     */
+    public function getCMSFields()
+    {
+        $self = $this;
+        $codes = DiscountCode::getValidCodes();
+
+        $this->beforeUpdateCMSFields(function ($fields) use ($self) {
+            if (!$self->isInDB()) {
+                $codes = DiscountCode::getValidCodes();
+                $fields->replaceField(
+                    'Code',
+                    DropdownField::create('Code', $this->fieldLabel('Code'))
+                        ->setSource($codes->map('Code', 'Title'))
+                );
+            }
+        });
+
+        return parent::getCMSFields();
+    }
+
+    /**
+     * If AppliedDiscount is set with code and no title/value set these up
+     * 
+     * @return null 
+     */
+    public function onBeforeWrite()
+    {
+        parent::onBeforeWrite();
+
+        if (empty($this->Title) || empty($this->Value)) {
+            $discount = $this->getDiscount();
+            $this->Title = $discount->Title;
+            $this->updateDiscount(false);
+        }
     }
 }
