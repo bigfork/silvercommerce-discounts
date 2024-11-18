3a193862cc464591707528d78cfe7100afe272b2
Loz Calver <kinglozzer@gmail.com>
2024-11-18 12:16:29 +0000

    Update DiscountCodeForm.php

--- src/forms/DiscountCodeForm.php
+++ src/forms/DiscountCodeForm.php
@@ -2,6 +2,7 @@
 
 namespace SilverCommerce\Discounts\Forms;
 
+use Exception;
 use SilverStripe\Forms\Form;
 use SilverStripe\Forms\FieldList;
 use SilverStripe\Forms\TextField;
@@ -27,7 +28,7 @@ class DiscountCodeForm extends Form
 
     public function __construct(
         RequestHandler $controller = null,
-        $name = self::DEFAULT_NAME,
+        $name,
         Estimate $estimate
     ) {
         $this->estimate_id = $estimate->ID;
@@ -35,10 +36,10 @@ class DiscountCodeForm extends Form
         $fields = FieldList::create(
             TextField::create(
                 "DiscountCode",
-                _t("ShoppingCart.DiscountCode", "Discount Code")
+                _t("Discounts.HaveDiscountCode", "Do you have a Discount Code")
             )->setAttribute(
                 "placeholder",
-                _t("ShoppingCart.EnterDiscountCode", "Enter a discount code")
+                _t("Discounts.EnterDiscountCode", "Enter discount code here")
             )
         );
 
@@ -80,30 +81,14 @@ class DiscountCodeForm extends Form
      */
     public function doAddDiscount($data, $form)
     {
-        $code_to_search = $data['DiscountCode'];
-        $cart = ShoppingCartFactory::create();
-        $existing = $cart->getOrder()->Discounts();
-        $limit = Config::inst()->get(ShoppingCartFactory::class, 'discount_limit');
-
-        if ($limit && $existing->exists() && $existing->count() >= $limit) {
-            $form->sessionMessage("Only ".$limit." discount code(s) can be used at a time.", 'bad');
-            return $this->getRequestHandler()->redirectBack();
-        }
-
-        $discount = DiscountFactory::create()->getByIdent($code_to_search);
-        
-        if (!$discount) {
-            $form->sessionMessage("The entered code is invalid.", 'bad');
-        } else {
+        try {
+            $code_to_search = $data['DiscountCode'];
             $estimate = $this->getEstimate();
-            // First check if the discount is already added (so we don't
-            // query the DB if we don't have to).
-            if (!$estimate->findDiscount($code_to_search)) {
-                DiscountFactory::create()->generateAppliedDiscount($code_to_search, $estimate);
-                $cart->save();
-            }
+            DiscountFactory::create($code_to_search, $estimate)->applyDiscountToEstimate();
+        } catch (Exception $e) {
+            $form->sessionMessage($e->getMessage(), 'bad');
         }
-        
+
         return $this->getRequestHandler()->redirectBack();
     }
 }
